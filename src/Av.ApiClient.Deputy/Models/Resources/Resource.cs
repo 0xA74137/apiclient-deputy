657e558ab0c34b7efddd97ad9c315c6e3cb12de7
0xA74137 <oscarlatorre@pm.me>
2024-01-14 00:46:29 +0100

    chore: remove unused class

--- src/Av.ApiClient.Deputy/Models/Resources/Resource.cs
+++ src/Av.ApiClient.Deputy/Models/Resources/Resource.cs
@@ -13,20 +13,4 @@ namespace Av.ApiClients.Deputy.Models.Resources
         DateTimeOffset? Created { get; set; }
         DateTimeOffset? Modified { get; set; }
     }
-
-    public class Resource : IResource
-    {
-        public long? Id { get; set; }
-        public DateTimeOffset? Created { get; set; }
-        public DateTimeOffset? Modified { get; set; }
-
-        protected bool SetField<T>(ref T field, T value, string name)
-        {
-            if(EqualityComparer<T>.Default.Equals(field, value))
-                return false;
-
-            field = value;
-            return true;
-        }
-    }
 }
